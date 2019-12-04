6cdafadb868b4390d2147c3b0eb4e430801fa902
Araf Hossain <arafh13@gmail.com>
2019-12-03 19:49:28 -0500

    Updates

---
+++ precache-manifest.7a55fd3a008db920cf5f3196f88b5c22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af18ddbc02be23bcd581fcb32a439dbd",
+    "url": "/Face-Recognition-FrontEnd/index.html"
+  },
+  {
+    "revision": "153eb6f3f09648c45f26",
+    "url": "/Face-Recognition-FrontEnd/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5c01b6643921aebdbfe3",
+    "url": "/Face-Recognition-FrontEnd/static/css/main.845cae69.chunk.css"
+  },
+  {
+    "revision": "153eb6f3f09648c45f26",
+    "url": "/Face-Recognition-FrontEnd/static/js/2.4102f985.chunk.js"
+  },
+  {
+    "revision": "5c01b6643921aebdbfe3",
+    "url": "/Face-Recognition-FrontEnd/static/js/main.2994cd21.chunk.js"
+  },
+  {
+    "revision": "df1d7b554d53fda95d62",
+    "url": "/Face-Recognition-FrontEnd/static/js/runtime-main.47392e3b.js"
+  }
+]);
